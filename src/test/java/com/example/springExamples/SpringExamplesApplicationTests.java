47ffc2c700184fe959c7e8d466401b8ef6f686f6
saketsharma <ricky.saket@gmail.com>
2025-03-23 12:01:29 -0500

    Spring Boot Example

---
+++ src/test/java/com/example/springExamples/SpringExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springExamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
